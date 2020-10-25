a698d74c5544aa2c0318c06ab3e5667df31e1357
FelipeLirao <felipe.lirao@hotmail.com>
2020-10-25 17:00:08 -0300

    Ajuste no jpa

--- chamado-service/src/main/java/com/tdc/chamado/entity/ChamadoVO.java
+++ chamado-service/src/main/java/com/tdc/chamado/entity/ChamadoVO.java
@@ -2,11 +2,9 @@ package com.tdc.chamado.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class ChamadoVO {
 	
 	@JsonProperty("id_usuario")
